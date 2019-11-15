b19a34b1eae00fdbd7b4b52cb4e0fdefe9763ab0
Kat <hello@katherinewharton.co.uk>
2019-11-15 21:15:40 +0000

    nav fade

--- new/joby/js/nav-fade.js
+++ new/joby/js/nav-fade.js
@@ -3,6 +3,6 @@ var myElement = $('.header');
 $(window).on('scroll', function () {
   var st = $(this).scrollTop();
   myElement.css({
-    'opacity': 1 - st / 100
+    'opacity': 1 - st / 100 !important
   });
 });
